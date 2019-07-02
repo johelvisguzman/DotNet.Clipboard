0dad4c445ca720368fb5e523c12a1c1059307bed
Johelvis Guzman <johelvisguzman@gmail.com>
2019-07-02 15:37:34 -0400

    Added ability to group clips by name

---
+++ src/DotNet.Clipboard/Behaviors/ContextMenuOnLeftClickBehavior.cs
@@ -0,0 +1,34 @@
+﻿namespace DotNet.Clipboard.Behaviors
+{
+    using System.Windows;
+    using System.Windows.Controls.Primitives;
+    using System.Windows.Interactivity;
+
+    public class ContextMenuOnLeftClickBehavior : Behavior<ButtonBase>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Click += OnClick;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.Click -= OnClick;
+            base.OnDetaching();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var contextMenu = AssociatedObject.ContextMenu;
+            if (contextMenu == null)
+            {
+                return;
+            }
+
+            contextMenu.PlacementTarget = AssociatedObject;
+            contextMenu.Placement = PlacementMode.Bottom;
+            contextMenu.IsOpen = true;
+        }
+    }
+}
